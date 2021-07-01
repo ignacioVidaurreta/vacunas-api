c60636a821561c5696ae00e9aa2746a0742d5eb7
Lautaro Pinilla <lpinilla@itba.edu.ar>
2021-06-30 21:43:43 -0300

    Agregada comando para descargar el a desagregated_shorterer.sh

--- desagregated_shorterer.sh
+++ desagregated_shorterer.sh
@@ -1,2 +1,3 @@
+wget https://sisa.msal.gov.ar/datos/descargas/covid-19/files/datos_nomivac_covid19.zip
 unzip datos_nomivac_covid19.zip
 cat datos_nomivac_covid19.csv | awk -F "," '{ print $11" "$12 }' | sort | uniq -c | sort -bnr | awk -F " " '{ print $2","$3","$1 }' > dates_vaccines_qty.csv
