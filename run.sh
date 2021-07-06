435f7bd7ea545a4df6f706efbff9534ef859c9b6
Lautaro Pinilla <lpinilla@itba.edu.ar>
2021-07-06 19:59:02 -0300

    correcciones menores

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 echo -e "\033[92m Descargando y transformando dataset\033[0m"
 #./desagregated_shorterer.sh
-echo -e "\033[92m Iniciando back\033[0m"
-uvicorn main:app --reload & > uvicorn_log
-echo -e "\033[92m Iniciando front\033[0m"
-&& cd dash; python3 app.py
+echo -e "\033[92m Iniciando front y back\033[0m"
+uvicorn main:app --reload & > uvicorn_log && cd dash; python3 app.py
