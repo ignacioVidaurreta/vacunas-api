28e28f7881227d50329611051c08895a45d500fc
Lautaro Pinilla <lpinilla@itba.edu.ar>
2021-07-06 20:25:19 -0300

    Arreglado porcentaje de poblaciones

--- utils.py
+++ utils.py
@@ -13,7 +13,7 @@ poblaciones_provincias = {
     '46': 398648,
     '50': 2010363,
     '54': 1274992,
-    '58': 672.461,
+    '58': 672461,
     '62': 757052,
     '66': 1441988,
     '70': 789489,
@@ -22,7 +22,7 @@ poblaciones_provincias = {
     '82': 3563390,
     '86': 988245,
     '90': 1714487,
-    '94': 177.697
+    '94': 177697
 }
 
 def grab_info_by_state(df, dose_col):
@@ -42,7 +42,7 @@ def grab_info_by_state(df, dose_col):
     for i in range(0, len(by_state)):
         row = list(by_state.iloc[i].to_numpy())
         vac_qty = row[2] if dose_col == "primera_dosis_cantidad" else row[3]
-        vac_qty /=  poblaciones_provincias[str(row[0])]
+        vac_qty /=  poblaciones_provincias[str(row[0])] * 100
         data = [*row[0:2], vac_qty]
         data[0] = int(data[0])
         data[2] = int(data[2])
