14cfdc4bbb5427b44c823cc813eb0ee9faa5068a
Lautaro Pinilla <lpinilla@itba.edu.ar>
2021-07-06 20:29:13 -0300

    Arreglado porcentaje de utils

--- utils.py
+++ utils.py
@@ -42,7 +42,7 @@ def grab_info_by_state(df, dose_col):
     for i in range(0, len(by_state)):
         row = list(by_state.iloc[i].to_numpy())
         vac_qty = row[2] if dose_col == "primera_dosis_cantidad" else row[3]
-        vac_qty /=  poblaciones_provincias[str(row[0])] * 100
+        vac_qty = float(vac_qty / poblaciones_provincias[str(row[0])] * 100)
         data = [*row[0:2], vac_qty]
         data[0] = int(data[0])
         data[2] = int(data[2])
