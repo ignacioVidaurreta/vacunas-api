5edbd019a578c7e60134b72b7654a9fbc6bbff65
nachito <igvidaurreta@gmail.com>
2021-07-06 23:05:54 -0300

    🔨 CHORE: download csv instead of xls

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ def _fetch_data():
 
 def fetch_recepcion_de_vacunas():
     print("Reading reception data")
-    url = "http://datos.salud.gob.ar/dataset/7e69d4b4-535d-4d4d-ad4a-362b6a1f4468/resource/d2851fa6-b105-4f15-b352-dd3d792bd526/download/2021-06-29-actas-de-recepcion-vacunas.xlsx"
-    df = pd.read_excel(url)
+    # url = "http://datos.salud.gob.ar/dataset/7e69d4b4-535d-4d4d-ad4a-362b6a1f4468/resource/d2851fa6-b105-4f15-b352-dd3d792bd526/download/2021-06-29-actas-de-recepcion-vacunas.xlsx"
+    # df = pd.read_excel(url)
+    df = pd.read_csv("2021-06-29-actas-de-recepcion-vacunas.csv")
     print("Finish Reading")
     return df
 
--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 echo -e "\033[92m Descargando y transformando dataset\033[0m"
 #./desagregated_shorterer.sh
+echo -e "\033[92m Descargando informacion de recepcion de vacunas\033[0m"
+./download_vac_reception.sh
 echo -e "\033[92m Iniciando front y back\033[0m"
 uvicorn main:app --reload & > uvicorn_log && cd dash; python3 app.py
