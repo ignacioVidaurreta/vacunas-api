381bd2a8e422c1bbe61ea8f94fd73c48cd7f75b4
Mica <mbanfi@itba.edu.ar>
2021-07-07 10:19:05 -0300

    📝 Undoing file download comment

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 echo -e "\033[92m Descargando y transformando dataset\033[0m"
-#./desagregated_shorterer.sh
+./desagregated_shorterer.sh
 echo -e "\033[92m Descargando informacion de recepcion de vacunas\033[0m"
 ./download_vac_reception.sh
-echo -e "\033[92m Iniciando front y back\033[0m"
-uvicorn main:app --reload & > uvicorn_log && cd dash; python3 app.py
+echo -e "\033[92m Iniciando back\033[0m"
+uvicorn main:app --reload & > uvicorn_log
+echo -e "\033[92m Iniciando front\033[0m"
+sleep 2
+cd dash; python3 app.py
