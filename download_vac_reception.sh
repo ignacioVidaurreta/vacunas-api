aee787da5907ffbf9488a1d4d38734e49666b52a
nachito <igvidaurreta@gmail.com>
2021-07-06 23:07:39 -0300

    🔨 FIX: add missing script

---
+++ download_vac_reception.sh
@@ -0,0 +1,3 @@
+ wget -q --show-progress http://datos.salud.gob.ar/dataset/7e69d4b4-535d-4d4d-ad4a-362b6a1f4468/resource/d2851fa6-b105-4f15-b352-dd3d792bd526/download/2021-06-29-actas-de-recepcion-vacunas.xlsx
+ libreoffice --headless --convert-to csv 2021-06-29-actas-de-recepcion-vacunas.xlsx
+
